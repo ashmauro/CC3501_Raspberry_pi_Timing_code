48a50bf0d234424bbd756ce3fa72fe4296ad35f0
ashmauro <87747971+ashmauro@users.noreply.github.com>
2022-10-24 18:46:57 +1000

    implement time on screen

--- Assignment_2_pi_code/src/gate_code.cpp
+++ Assignment_2_pi_code/src/gate_code.cpp
@@ -29,7 +29,8 @@ using namespace std;
 using namespace std::chrono;
 using namespace cv;
 #define BUF_SIZE 100
-cv::Mat bgr_img;
+Mat bgr_img;
+Mat display_img;
 
 //fun fact run ls /dev/tty* on the pi console to see connected ports, looking for ttyACM0/1/2
 //TODO pop out window
@@ -38,6 +39,7 @@ cv::Mat bgr_img;
 // gobal vars used in the functions
 int gate_id; //used ot assign gate functions
 bool start_to_finish = false; // used to switch the function of the starting gate to a finish line
+bool driver_bool = false;
 
 // values used to record timing CURRENTLY used in the internal functions
 auto start_gate = high_resolution_clock::now();
@@ -63,9 +65,15 @@ int second_serial_buf_id = 0;
 char third_serial_buf [BUF_SIZE];
 int third_serial_buf_id = 0;
 
+int j = 0;
+int driver_index;
+int sect1_time;
+int sect2_time;
+int sect3_time;
+int laptime;
 
 // this function handles how a gate functions, depending on its assigned gate id
-bool gate_logic (int gate_id, bool start_to_finish) {
+bool gate_logic (struct driver driver_array[10], int gate_id, bool start_to_finish) {
     switch (gate_id) 
         {
             case 1: // its the start/finish gate
@@ -77,7 +85,13 @@ bool gate_logic (int gate_id, bool start_to_finish) {
                     printf("Sector 3 Time: %d\n", (int)sector_3.count()); // type cast the var.count to int to use it elswhere otherwise will default to zero.
                     printf("Total Time: %d\n", (int)total_time.count());
                     printf("Race Ended\n");
-                    // present_leaderboard((int)sector_1.count(), (int)sector_2.count(), (int)sector_3.count(), (int)total_time.count()); // Present data to leaderboard
+                    sect3_time = (int)sector_3.count();
+                    laptime = (int)total_time.count();
+
+                    // printf("Enter next drivers name:\n");
+                    // driver_bool = true;
+                    // j = 2;
+                    // get_name(driver_bool, j);
                 }
                 else if(start_to_finish == false){ // if the gate is in its "start" state
                     printf("RACE START\n"); 
@@ -88,14 +102,14 @@ bool gate_logic (int gate_id, bool start_to_finish) {
                 second_gate = high_resolution_clock::now(); // get second gate time
                 sector_1 = duration_cast<milliseconds>(second_gate - start_gate); //calc the time in sector 1
                 printf("Sector 1 Time: %d\n", (int)sector_1.count());
-                // present_leaderboard((int)sector_1.count(), 0, 0, 0); // present leaderboard
+                sect1_time = (int)sector_1.count();
                 break;
             case 3: // its the third gate
                 start_to_finish = true; //set the start gate to finish
                 third_gate = high_resolution_clock::now(); //get the third gate time
                 sector_2 = duration_cast<milliseconds>(third_gate - second_gate); // calc the time in sector 2
                 printf("Sector 2 Time: %d\n", (int)sector_2.count());
-                // present_leaderboard((int)sector_1.count(), (int)sector_2.count(), 0, 0);
+                sect2_time = (int)sector_2.count();
                 break;
             default:
                 break;
@@ -130,6 +144,7 @@ int process_first_buffer(char string_data, size_t bytes_read) {// handles the fi
     {   // -1 to allow room for the NULL
         // Store the result
         first_serial_buf[first_serial_buf_id++] = string_data;
+        cout << first_serial_buf << " tty0 "<< endl;
         // test the buffer value to see if it contains an a, b or c, then change gate_id_value accordinly
         if (strcmp("a",first_serial_buf) == 0) 
         {
@@ -167,6 +182,7 @@ int process_second_buffer(char string_data, size_t bytes_read) {
     else if (second_serial_buf_id < BUF_SIZE-1) 
     {
         second_serial_buf[second_serial_buf_id++] = string_data;
+        cout << second_serial_buf << " tty1 "<< endl;
         if (strcmp("a",second_serial_buf) == 0) 
         {
             gate_id_value = 1;
@@ -205,7 +221,8 @@ int process_third_buffer(char string_data, size_t bytes_read) {
     else if (third_serial_buf_id < BUF_SIZE-1) 
     {   // -1 to allow room for the NULL
         // Store the result
-        second_serial_buf[third_serial_buf_id++] = string_data;
+        third_serial_buf[third_serial_buf_id++] = string_data;
+        cout << third_serial_buf <<" tty2 "<< endl;
         if (strcmp("a",third_serial_buf) == 0) 
         {
             gate_id_value = 1;
@@ -222,8 +239,38 @@ int process_third_buffer(char string_data, size_t bytes_read) {
     return gate_id_value;
 }
 
-void present_leaderboard(string driver_name, int sect1_time, int sect2_time, int sect3_time, int laptime) {
+// Create struct for driver stats
+struct driver {
+    string driver_name;
+    int sector1_time;
+    int sector2_time;
+    int sector3_time;
+    int lap_time;
+    int index;
+};
+
+void update_stats(struct driver driver_array[10], int sect1_time, int sect2_time, int sect3_time, int laptime, int index) {
     
+    // Update drivers stat
+    driver_array[index].sector1_time = sect1_time;
+    driver_array[index].sector2_time = sect2_time;
+    driver_array[index].sector3_time = sect3_time;
+    driver_array[index].lap_time = laptime;
+}
+
+void update_name(struct driver driver_array[10], string driver_name, int index) {
+    
+    // Update drivers stat
+    driver_array[index].driver_name = driver_name;
+    
+}
+
+void present_leaderboard(struct driver driver_array[10]) {
+    int i = 0;
+    display_img = bgr_img.clone();
+    cv::imshow("Leaderboard", display_img);
+    cv::waitKey(1000);
+
     // A difference of 200 in the x position for columns
     // A difference of 60 in the y position for each row
 
@@ -231,47 +278,85 @@ void present_leaderboard(string driver_name, int sect1_time, int sect2_time, int
     // Will need to sort by lap time and then loop through and putText of corresponding info.
 
     // Convert times to strings
-    string sect1_str = to_string(sect1_time);
-    string sect2_str = to_string(sect2_time);
-    string sect3_str = to_string(sect3_time);
-    string lapt_str = to_string(laptime);
-
-    // Put text
-    cv::putText(bgr_img,
-                driver_name,
+    string sect1_str = to_string(driver_array[i].sector1_time);
+    string sect2_str = to_string(driver_array[i].sector2_time);
+    string sect3_str = to_string(driver_array[i].sector3_time);
+    string lapt_str = to_string(driver_array[i].lap_time);
+    
+    // Put driver name
+    cv::putText(display_img,
+                driver_array[i].driver_name,
                 cv::Point(175, 350), //text position
                 cv::FONT_HERSHEY_DUPLEX,
                 1.0, // font scale
                 CV_RGB(255, 255, 255), //font color
                 2);
 
-    // Put text
-    cv::putText(bgr_img,
+    // Put ector 1 time
+    cv::putText(display_img,
                 sect1_str,
                 cv::Point(375, 350), //text position
                 cv::FONT_HERSHEY_DUPLEX,
                 1.0, // font scale
                 CV_RGB(255, 255, 255), //font color
                 2);
-    // Put text
-    cv::putText(bgr_img,
+    // Put sector 2 time
+    cv::putText(display_img,
                 sect2_str,
-                cv::Point(575, 410), //text position
+                cv::Point(575, 350), //text position
                 cv::FONT_HERSHEY_DUPLEX,
                 1.0, // font scale
                 CV_RGB(255, 255, 255), //font color
                 2);
     
-    cv::imshow("Leaderboard", bgr_img); //show the leaderboard
+    // Put sector 3 time
+    cv::putText(display_img,
+                sect3_str,
+                cv::Point(775, 350), //text position
+                cv::FONT_HERSHEY_DUPLEX,
+                1.0, // font scale
+                CV_RGB(255, 255, 255), //font color
+                2);
+    
+    // Put total laptime
+    cv::putText(display_img,
+                lapt_str,
+                cv::Point(975, 350), //text position
+                cv::FONT_HERSHEY_DUPLEX,
+                1.0, // font scale
+                CV_RGB(255, 255, 255), //font color
+                2);
+    
+    cv::imshow("Leaderboard", display_img); //show the leaderboard
     
     // Allow openCV to process GUI events
     cv::waitKey(1000);
 	
 }
 
+void get_name(bool driver_bool, int j) {
+
+    if (driver_bool == true) {
+       switch (j) { // Get drivers name
+            case 1:
+                printf("Please enter drivers name:\n");
+               
+            break;
+            case 2:
+                printf("Please enter next drivers name:\n");
+                driver_index++;
+            break;
+        }
+    
+        j = 0;
+    }
+}
 
 int main(int argc, char *argv[])  {
     
+    driver driver_array[10];
+    
+
     // Load the LeaderBoard image
 	bgr_img = imread("../leaderboard.jpg");
 	if (!bgr_img.data) {
@@ -281,11 +366,10 @@ int main(int argc, char *argv[])  {
 			return 1;
 		}
 	}
+    
 
     // not a bug, its a feature :)
     printf("Please configure all gates by moving your hand through each gate :)\n");
-    // Get drivers name
-    printf("Please enter drivers name:\n");
 
     // sets values to check if all the gate are configured
     enum gate_state_t {not_configd = 0, configd = 1}; 
@@ -337,16 +421,20 @@ int main(int argc, char *argv[])  {
     };
 
     static char user_msg [512];
+    // Variables for driver stats
     for (;;) {
-
+        
         // Wait for events
         poll(pfds, sizeof(pfds)/sizeof(struct pollfd), -1);
         // Check if a a port has sent info arrived
 
         // tests if all gate are configd, currently only test ptt0/1, replace if test stamtent with commented code to test all
-        if ((port_ttyACM0_state == configd) && (port_ttyACM1_state == configd)) // (port_ttyACM0_state == configd) && (port_ttyACM1_state == configd) && (port_ttyACM2_state == configd)
+        if ((port_ttyACM0_state == configd) && (port_ttyACM1_state == configd) && (port_ttyACM2_state == configd) && (all_ports_state == not_configd)) // (port_ttyACM0_state == configd) && (port_ttyACM1_state == configd) && (port_ttyACM2_state == configd)
         {
             all_ports_state = configd;
+            driver_bool = true;
+            j = 1;
+            get_name(driver_bool, j);
         }
 
         if (pfds[0].revents) // port tty0 gets an event 
@@ -367,6 +455,7 @@ int main(int argc, char *argv[])  {
             }
             else if(all_ports_state == configd)
             {
+                // printf("tty0\n");
                 //all gates are configd
                 char string_data;//NEEDED DO NOT MOVE
                 size_t bytes_read = read(port_ttyACM0, &string_data, 1);//NEEDED DO NOT MOVE
@@ -396,6 +485,7 @@ int main(int argc, char *argv[])  {
             }
             else if(all_ports_state == configd)
             {
+                // printf("tty1\n");
                 char string_data;
                 size_t bytes_read = read(port_ttyACM1, &string_data, 1);
                 // char string_data;
@@ -415,7 +505,7 @@ int main(int argc, char *argv[])  {
                 char string_data;
                 size_t bytes_read = read(port_ttyACM2, &string_data, 1);
                 printf("Gate configured\n");
-                port_ttyACM1_state = configd;
+                port_ttyACM2_state = configd;
                 
                 if (bytes_read == 0) 
                 {
@@ -425,6 +515,7 @@ int main(int argc, char *argv[])  {
             }
             else if(all_ports_state == configd)
             {
+                // printf("tty2\n");
                 char string_data;
                 size_t bytes_read = read(port_ttyACM2, &string_data, 1);
                 // char string_data;
@@ -438,28 +529,43 @@ int main(int argc, char *argv[])  {
                     
         }
         
+        // Initiliase new driver in the Drivers class array
         if (pfds[3].revents) {
-            // Read the incoming message from terminal
-            ssize_t bytes_read = read(STDIN_FILENO, user_msg, sizeof(user_msg) - 1); // with room for a trailing null
-            if (bytes_read < 0) {
-                return 0;
-            }
-            // Make the message null terminated
-            user_msg[bytes_read] = 0;
-            size_t drivername_length = strlen(user_msg);
-
-            printf("Goodluck for your race %s!! \n", user_msg);
-
             
-            string driver_name;
-            driver_name.assign(user_msg, user_msg + (drivername_length - 1));
-            present_leaderboard(driver_name, 100, 101, 103, 304);
+            if (driver_bool == true) {
+            
+                // Read the incoming message from terminal
+                ssize_t bytes_read = read(STDIN_FILENO, user_msg, sizeof(user_msg) - 1); // with room for a trailing null
+                
+                if (bytes_read < 0) {
+                    return 0;
+                }
+                
+                // Increment index
+                // Make the message null terminated
+                user_msg[bytes_read] = 0;
+                size_t drivername_length = strlen(user_msg);
+                printf("Goodluck for your race %s!! \n", user_msg);
 
+                // sect1_time = 0;
+                // sect2_time = 0;
+                // sect3_time = 0;
+                // laptime = 0;
+                driver_index = 0;
+
+                string drivername;
+                drivername.assign(user_msg, user_msg + (drivername_length - 1));
+                update_name(driver_array, drivername, driver_index);
+                update_stats(driver_array, sect1_time, sect2_time, sect3_time, laptime, driver_index);
+                present_leaderboard(driver_array);
+                
+            }
+            driver_bool = false;
         }
         
         // functionality of each gate is here! //TODO TURN INTO FUNCTION
-        start_to_finish = gate_logic(gate_id, start_to_finish);
-
+        start_to_finish = gate_logic(driver_array, gate_id, start_to_finish);
+        update_stats(driver_array, sect1_time, sect2_time, sect3_time, laptime, driver_index);
         gate_id = 0;
     }
 }
